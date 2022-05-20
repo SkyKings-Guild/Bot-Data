b32012eef87d137c853737b3d74f11ae2dc60208
plun1331 <plun1331@gmail.com>
2022-05-19 17:37:09 -0700

    add some output

--- check.py
+++ check.py
@@ -6,3 +6,4 @@ for _dir in [f.path.replace('\\', '/') for f in os.scandir(".") if f.is_dir()]: 
         if file.endswith(".json"):
             with open(_dir + '/' + file, 'r', encoding='utf-8') as f:
                 json.load(f)
+                print(_dir + '/' + file, 'OK')
