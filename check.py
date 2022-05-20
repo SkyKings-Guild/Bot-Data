a040ec0f666e79955276a0e5ca732ae9685baa2a
plun1331 <plun1331@gmail.com>
2022-05-19 17:34:47 -0700

    fix

--- check.py
+++ check.py
@@ -1,7 +1,8 @@
 import json
 import os
 
-for _dir in [f.path.replace('\\', '/') for f in os.scandir("extensions") if f.is_dir()]:  # type: ignore
+for _dir in [f.path.replace('\\', '/') for f in os.scandir(".") if f.is_dir()]:  # type: ignore
     for file in os.listdir(_dir):
-        with open(_dir + '/' + file, 'r', encoding='utf-8') as f:
-            json.load(f)
+        if file.endswith(".json"):
+            with open(_dir + '/' + file, 'r', encoding='utf-8') as f:
+                json.load(f)
